232390b48dee7946fb57a4fd7409e2170e6a4b69
riemer <riemer.domingo0729@gmail.com>
2023-10-13 15:31:19 +0800

    first commit

---
+++ for/for.php
@@ -0,0 +1,22 @@
+<?php
+	
+	echo "<br><br><br>";
+
+	for ($num=1; $num <=20 ; $num++) { 
+	echo $num . ",";
+	}
+
+	echo "<br><br><br>";
+
+	for ($num=1; $num <=20 ; $num++) { 
+		echo $num;
+		if ($num !=20) {
+			echo ",";
+		} else {
+			break;
+		}
+		
+	}
+
+	?>
+
